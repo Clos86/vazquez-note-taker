d2fe0e6f209c06a5ee613b8c84d88640b7310a9f
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-07 20:58:33 -0500

    working on post

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const notes = require('./db/db');
+
 const PORT = process.env.PORT || 3001;
+const notes = require('./db/db');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// API Routes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    res.json(req.body);
 });
 
 // HTML Routes
