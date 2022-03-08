4d649d69deb5af6163510225da7db2542cfd60ce
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-07 19:52:15 -0500

    added HTML routes

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// HTML Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+  
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// App listener
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
