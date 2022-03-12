99071a617e9af770667ed97e76d607d81d4ac5c7
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-12 17:03:52 -0500

    edit HTML routes

--- server.js
+++ server.js
@@ -32,8 +32,12 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
   
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // App listener
