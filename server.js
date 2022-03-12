d4b866319048f2205e297645f935b5f9eea58e85
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-12 17:07:12 -0500

    more edits to HTML routes

--- server.js
+++ server.js
@@ -27,13 +27,13 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
-// HTML Routes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-  
+// HTML Routes 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
